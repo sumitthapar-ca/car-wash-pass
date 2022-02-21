d2c21b3166fff8a700db07dbaa500258e8273475
Sumit Thapar <sumitthapar.ca@gmail.com>
2022-02-20 21:57:47 -0500

    rename changes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from art import car
-from wash_pass import WashPass
+from car_wash_pass import CarWashPass
 import os
 from datetime import *
 from dateutil import parser
 
 
-wash_pass = WashPass()
+car_wash_pass = CarWashPass()
 
 os.system('clear')
 print('##############################################################')
@@ -16,17 +16,17 @@ print('##############################################################\n')
 print(car)
 
 car_wash_price = input("Enter the price for car wash pass: ")
-wash_pass.set_carwash_price(car_wash_price)
+car_wash_pass.set_carwash_price(car_wash_price)
 
 members = int(input("\nEnter total no of members: "))
 
 for i in range(1,members + 1):
     member_name = input(f"\nEnter the name of member {i}: ")
-    wash_pass.set_member(member_name)
+    car_wash_pass.set_member(member_name)
 
-wash_pass.get_members()
+car_wash_pass.get_members()
 
 start_date = parser.parse(input("\nPlease enter the start date for the pass(MM/DD/yyyy): "))
-wash_pass.set_pass_startend_dates(start_date)
+car_wash_pass.set_pass_startend_dates(start_date)
 
-wash_pass.print_member_days()
+car_wash_pass.print_member_days()
