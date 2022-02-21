2f0b8cd3f6e9d9ff9228f2fc685062fcd753385a
Sumit Thapar <sumitthapar.ca@gmail.com>
2022-02-20 21:52:41 -0500

    calculate days for members

--- main.py
+++ main.py
@@ -15,7 +15,18 @@ print('#                                                            #')
 print('##############################################################\n')
 print(car)
 
-wash_pass.print_carwash_price()
-wash_pass.set_members(5)
-start_date = parser.parse(input("Please enter the start date for the pass(MM/DD/yyyy)"))
+car_wash_price = input("Enter the price for car wash pass: ")
+wash_pass.set_carwash_price(car_wash_price)
+
+members = int(input("\nEnter total no of members: "))
+
+for i in range(1,members + 1):
+    member_name = input(f"\nEnter the name of member {i}: ")
+    wash_pass.set_member(member_name)
+
+wash_pass.get_members()
+
+start_date = parser.parse(input("\nPlease enter the start date for the pass(MM/DD/yyyy): "))
 wash_pass.set_pass_startend_dates(start_date)
+
+wash_pass.print_member_days()
--- wash_pass.py
+++ wash_pass.py
@@ -2,24 +2,45 @@ from datetime import date
 from dateutil.relativedelta import *
 
 class WashPass:
-    price = 199
-    members = 0
+    price = 0
+    members = []
 
     def __init__(self):
         self.price = (.13 * self.price) + self.price 
 
-    def print_carwash_price(self):
-        """Print the price of the car wash pass"""
-        print(f'The price of the car wash pass is: {self.price}\n')
+    def set_carwash_price(self, car_wash_price):
+        """Set the price of the car wash pass"""
+        self.price = car_wash_price
+        # print(f'The price of the car wash pass is: {self.price}\n')
 
-    def set_members(self, members):
-        self.members = members
-        print(f'Total number of members in the pass: {self.members}\n')
-        print(f'Price per member: {round(self.price / self.members, 2)}\n')
+    def set_member(self, member):
+        """Set the no of members within the pass"""
+        self.members.append(member)
+        # print(f'Total number of members in the pass: {self.members}\n')
+        # print(f'Price per member: {round(self.price / self.members, 2)}\n')
     
+    def get_members(self):
+        for x in self.members:
+            print(x)
+
     def set_pass_startend_dates(self, start_date):
         """Set Start and End date for the Car wash pass"""
         self.start_date = start_date
         self.end_date = self.start_date + relativedelta(days=+90)
-        print(f"Car wash pass starts on {self.start_date.strftime('%b %d %Y')}")
-        print(f"Car wash pass ends on {(self.start_date + relativedelta(days=+90)).strftime('%b %d %Y')}")
+        print(f"\nCar wash pass starts on {self.start_date.strftime('%b %d %Y')}")
+        print(f"Car wash pass ends on {(self.end_date).strftime('%b %d %Y')}")
+        print(f"Total no of days: {(self.end_date - self.start_date).days}")
+    
+    def print_member_days(self):
+        days = (self.end_date - self.start_date).days
+
+        x=0
+        for i in range(1, days + 1):
+            date_user = self.start_date + relativedelta(days=+ (i-1))
+            print(f"{date_user.strftime('%b %d %Y')} is for {self.members[x]}")
+            x += 1
+            if x >= len(self.members):
+                x = 0
+                 
+            
+
